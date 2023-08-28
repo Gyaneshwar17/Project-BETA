d301e204951b988eceadcd421f7b93d62235710e
Gyan <igyaneshwarrao@gmail.com>
2023-08-28 21:46:15 +0530

    1st commit

---
+++ password.py
@@ -0,0 +1,19 @@
+k=[]
+print("Welcome To Password Generator: ")
+a=int(input("-> Enter the count of small and big\n   alphabets u want in ur password : "))
+b=int(input("-> Enter the count of number u want : "))
+c=int(input("-> Enter the count of speical  u want : "))
+print(f"Generating a strong password with {a} aphabets,{b} numbers ,{c} symbols...")
+import random
+for i in range(a):
+    d=random.randint(65,123)
+    k.append(chr(d))
+for j in range(b):
+    e=random.randint(48,58)
+    k.append(chr(e))
+for m in range(c):
+    f=random.randint(33,49)
+    k.append(chr(f))
+random.shuffle(k)
+for o in k:
+    print(o,end="")
